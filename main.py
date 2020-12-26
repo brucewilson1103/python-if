e3b3075f8cfe8d864e17aea2245670fe209237c6
Bruce Wilson <brucewilson1103@gmail.com>
2020-12-26 16:47:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height > 120:
+  print('You are good to go.')
+else:
+  print('Sorry come back next year.')
